319459327b2d06a6e55d534070a8b370972a090c
Dan Humphries <45633295+dnhmphrs@users.noreply.github.com>
2024-02-02 10:55:51 +0000

    tweak

--- vfx-breakdown/src/components/Plane.jsx
+++ vfx-breakdown/src/components/Plane.jsx
@@ -10,12 +10,14 @@ export default function Plane({ location = [0, 0, 0], imgSrc, cosScroll }) {
   // Calculate the initial zoom based on the maximum expected pan effect
   useMemo(() => {
     const pan_effect = 0.05;
-    const maxPanEffect = pan_effect; // The maximum extent of the pan effect
-    const initialZoom = 1.0 - maxPanEffect; // Ensure initial zoom accounts for max pan effect
     const parallaxFactor = location[2] - 4; // Use Z position as a factor for parallax intensity
-    texture.offset.x = parallaxFactor * cosScroll * 0.5 * pan_effect + 0.1; // Adjust for desired effect
+    texture.offset.x = parallaxFactor * cosScroll * 0.5 * pan_effect + 0.05; // Adjust for desired effect
     // Adjust initial zoom to counter the pan effect and apply additional zoom based on cosScroll
-    texture.repeat.set(initialZoom + Math.abs(parallaxFactor) * location[0] * (1 - cosScroll), initialZoom);
+
+    const zoomIntensity = 0.1;
+    const zoomEffect = 1 + Math.abs(cosScroll) * (location[2] - 1) * zoomIntensity;
+
+    texture.repeat.set(zoomEffect, zoomEffect);
 
   }, [cosScroll, location, texture]);
 
